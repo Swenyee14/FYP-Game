57a42d5022468f2a282daacea3793f6977c9a2b9
Swenyee Htut Maung <120029812+Swenyee14@users.noreply.github.com>
2025-04-30 14:24:15 +0100

    added gameover screen

--- Assets/Scripts/JunkManager.cs
+++ Assets/Scripts/JunkManager.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class JunkManager : MonoBehaviour
 {
     public static JunkManager instance;
     private int junkFood;
     [SerializeField] private TMP_Text showJunkFood;
+    [SerializeField] private GameObject gameOverScreen;
 
     private void Awake()
     {
@@ -25,5 +27,24 @@ public class JunkManager : MonoBehaviour
     public void ChangeJunk(int num)
     {
         junkFood += num;
+
+        if (junkFood == 15)
+        {
+            Debug.Log("failed");
+            gameOverScreen.SetActive(true);
+            Time.timeScale = 0f;
+        }
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Time.timeScale = 1f;
+    }
+
+    public void QuitGame()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(0);
     }
 }
